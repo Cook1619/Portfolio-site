efaaab79e3aa90baecf57139a2cc1ebef51f65b2
Cook1619 <cook6348@gmail.com>
2020-06-09 19:18:51 -0500

    Landing page getting planned out

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return(
-        <div>
-            Home Page
+  return (
+    <>
+      <div className="flex justify-center mb-4">
+        <div className="text-black-900 font-bold text-4xl mb-2">
+          Full Stack Web Developer
         </div>
-    )
-}
+      </div>
+      <div className="flex justify-center mb-4">
+        <div className="text-black text-lg mb-2">
+          Front end focused with a passion for{" "}
+          <span className="text-purple-700 font-bold">design</span> and{" "}
+          <span className="text-purple-700 font-bold">data</span>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+const Navbar = () => {
 
   return (
     <>
@@ -62,3 +62,5 @@ export default function Navbar() {
     </>
   );
 }
+
+export default Navbar;
