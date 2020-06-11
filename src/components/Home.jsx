170d9f29b6f785b10682caafa52f86c20455c5dc
Cook1619 <cook6348@gmail.com>
2020-06-10 19:24:25 -0500

    Adding animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import img from "../img/github_pic.jpeg";
+import '../styles/main.css'
 
 const Home = () => {
   return (
     <>
-      <div className="flex justify-center mb-4">
-        <div className="text-black-900 font-bold text-4xl mb-2">
-          Full Stack Web Developer
+      <div className="flex justify-center mb-4 fade-in">
+        <div className="text-grey-900 font-bold text-xl mb-2">
+          Hello, I'm Matt <span className="hand-wave" role="img" aria-label="hand-wave">👋</span>
+        </div>
+        <div className="h-48 w-48">
+          {/* <img
+            src={img}
+            alt="Matt Cook"
+          /> */}
         </div>
       </div>
-      <div className="flex justify-center mb-4">
+      <div className="flex justify-center mb-4 content-fade-in">
         <div className="text-black text-lg mb-2">
           Front end focused with a passion for{" "}
           <span className="text-purple-700 font-bold">design</span> and{" "}
